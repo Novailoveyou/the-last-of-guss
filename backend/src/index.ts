7f5ff48e310158159caa2f8480a7ab111aa3df4e
Nova <nova@Novas-MacBook-Pro.local>
2025-09-24 02:20:36 +0300

    feat: root route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import roundController from './modules/round/round.route.js'
 import pointController from './modules/point/point.route.js'
 
 const app = fastify({
-	logger: true,
+  logger: true,
 })
 
 await app.register(fastifyEnv, envOptions)
@@ -28,16 +28,20 @@ app.register(fastifyBcrypt, { saltWorkFactor: 12 })
 app.register(jwtPlugin)
 
 app.register(fastifyCookie, {
-	secret: app.getEnvs<Envs>().COOKIE_SECRET,
-	parseOptions: {
-		domain: app.getEnvs<Envs>().COOKIE_DOMAIN,
-		path: '/',
-		httpOnly: true,
-		secure: app.getEnvs<Envs>().NODE_ENV === 'production',
-		sameSite: 'strict',
-		maxAge: 24 * 60 * 60, // 1 day in seconds
-		signed: true,
-	},
+  secret: app.getEnvs<Envs>().COOKIE_SECRET,
+  parseOptions: {
+    domain: app.getEnvs<Envs>().COOKIE_DOMAIN,
+    path: '/',
+    httpOnly: true,
+    secure: app.getEnvs<Envs>().NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 24 * 60 * 60, // 1 day in seconds
+    signed: true,
+  },
+})
+
+app.get('/', async (request, reply) => {
+  return reply.code(200).send({ status: 'ok' })
 })
 
 app.register(survivorController, { prefix: '/survivors' })
@@ -47,9 +51,9 @@ app.register(roundController, { prefix: '/rounds' })
 app.register(pointController, { prefix: '/points' })
 
 await app.listen({ port: app.getEnvs<Envs>().PORT }, (err, address) => {
-	if (err) {
-		app.log.error(err)
-		process.exit(1)
-	}
-	app.log.info(`Server listening at ${address}`)
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+  app.log.info(`Server listening at ${address}`)
 })

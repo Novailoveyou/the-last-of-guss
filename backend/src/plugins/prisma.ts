499403cc88298c81d34b661e9e46628acc57d824
Nova <nova@Novas-MacBook-Pro.local>
2025-09-24 01:44:41 +0300

    feat: prisma accelerate

--- backend/src/plugins/prisma.ts
+++ backend/src/plugins/prisma.ts
@@ -1,23 +1,24 @@
 import fp from 'fastify-plugin'
 import type { FastifyPluginAsync } from 'fastify'
 import { PrismaClient } from '../../dist/generated/prisma/client.js'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare module 'fastify' {
-	interface FastifyInstance {
-		prisma: PrismaClient
-	}
+  interface FastifyInstance {
+    prisma: PrismaClient
+  }
 }
 
 const prismaPlugin: FastifyPluginAsync = fp(async (server, options) => {
-	const prisma = new PrismaClient()
+  const prisma = new PrismaClient().$extends(withAccelerate())
 
-	await prisma.$connect()
+  await prisma.$connect()
 
-	server.decorate('prisma', prisma)
+  server.decorate('prisma', prisma as unknown as any)
 
-	server.addHook('onClose', async (server) => {
-		await server.prisma.$disconnect()
-	})
+  server.addHook('onClose', async server => {
+    await server.prisma.$disconnect()
+  })
 })
 
 export default prismaPlugin

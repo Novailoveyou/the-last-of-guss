9648f8071932a4f3352e24dd9ce0497f027ec0ae
Nova <nova@Novas-MacBook-Pro.local>
2025-09-24 07:14:16 +0300

    fix: build

--- frontend/src/entities/survivor/model.ts
+++ frontend/src/entities/survivor/model.ts
@@ -1,9 +1,17 @@
 import z from 'zod'
-import { SurvivorModel } from '../../../../docs/generated/zod/survivor'
 
+// @TODO: apply zod schemas from the docs
 export const formSchema = z.object({
-  login: SurvivorModel.shape.login,
-  password: SurvivorModel.shape.password,
+  login: z
+    .string()
+    .trim()
+    .min(1, { message: 'Логин должен быть больше 1-го символа' })
+    .max(256, { message: 'Логин должен быть меньше 256-и символов' }),
+  password: z
+    .string()
+    .trim()
+    .min(5, { message: 'Пароль должен быть больше 5-и символов' })
+    .max(64, { message: 'Пароль должен быть меньше 64-ех символов' }),
 })
 
 export type FormSchema = z.infer<typeof formSchema>
